adb3e6da52f5f162b3134067798f8b648b8c2c8f
Ni Axi <niaxi@tuta.io>
2015-07-23 02:52:40 -0600

    Cleanup unused bits

--- src/config.js
+++ src/config.js
@@ -18,7 +18,6 @@ System.config({
     "babel": "npm:babel-core@5.6.15",
     "babel-runtime": "npm:babel-runtime@5.6.15",
     "core-js": "npm:core-js@0.9.18",
-    "css": "github:systemjs/plugin-css@0.1.13",
     "font-awesome": "npm:font-awesome@4.3.0",
     "normalize.css": "github:necolas/normalize.css@3.0.3",
     "github:jspm/nodelibs-process@0.1.1": {
