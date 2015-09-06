cc2f4896a3fb86959e5b850d600df0d9c0cec3c2
Ni Axi <niaxi@tuta.io>
2015-09-06 04:03:17 -0600

    Upgrade font-awesome from 4.3.0 t0 4.4.0

--- src/config.js
+++ src/config.js
@@ -18,13 +18,13 @@ System.config({
     "babel": "npm:babel-core@5.6.15",
     "babel-runtime": "npm:babel-runtime@5.6.15",
     "core-js": "npm:core-js@0.9.18",
-    "font-awesome": "npm:font-awesome@4.3.0",
+    "font-awesome": "npm:font-awesome@4.4.0",
     "normalize.css": "github:necolas/normalize.css@3.0.3",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
     "github:necolas/normalize.css@3.0.3": {
-      "css": "github:systemjs/plugin-css@0.1.13"
+      "css": "github:systemjs/plugin-css@0.1.16"
     },
     "npm:babel-runtime@5.6.15": {
       "process": "github:jspm/nodelibs-process@0.1.1"
@@ -34,8 +34,8 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
-    "npm:font-awesome@4.3.0": {
-      "css": "github:systemjs/plugin-css@0.1.13"
+    "npm:font-awesome@4.4.0": {
+      "css": "github:systemjs/plugin-css@0.1.16"
     }
   }
 });
